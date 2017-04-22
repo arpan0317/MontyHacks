11b9e8efd4ea6d4043fb063a0164254898b3d554
bho <brendon.ho01@gmail.com>
2017-04-22 15:45:53 -0400

    ayy

--- app/src/main/java/com/brendonho/studybuddy/Bot.java
+++ app/src/main/java/com/brendonho/studybuddy/Bot.java
@@ -16,6 +16,9 @@ import java.util.List;
 
 public class Bot extends AppCompatActivity {
 
+    private View mContentView;
+    private View mLoadingView;
+    private int mShortAnimationDuration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +27,10 @@ public class Bot extends AppCompatActivity {
 
         final EditText stuff = (EditText) findViewById(R.id.editText);
         Button send = (Button) findViewById(R.id.button);
-        TextView you = (TextView) findViewById(R.id.you);
+        final TextView you = (TextView) findViewById(R.id.you);
         final TextView bot = (TextView) findViewById(R.id.bot);
+
+
 
 
 
@@ -33,52 +38,96 @@ public class Bot extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String stuffin = stuff.getText().toString();
+                String youin = stuff.getText().toString();
                 String helper1234 = " ";
-                switch (stuffin){
-                    case "math": helper1234 = "What math course?";
+                String corr = "Correct";
+                String incorr = "Incorrect";
+                switch (stuffin) {
+                    case "math":
+                        helper1234 = "What math course?";
                         bot.setText(helper1234);
+                        you.setText("math");
+                        stuff.setText("");
                         break;
-                    case "science": helper1234 = "What science course?";
+                    case "science":
+                        helper1234 = "What science course?";
                         bot.setText(helper1234);
+                        you.setText("science");
+                        stuff.setText("");
                         break;
-                    case "english": helper1234 = "What topic in english?";
+                    case "english":
+                        helper1234 = "What topic in english?";
                         bot.setText(helper1234);
+                        you.setText("english");
+                        stuff.setText("");
                         break;
-                    default:helper1234 = "Not understood";
+                    case "precalculus":
+                        helper1234 = "What precalculus course?";
+                        bot.setText(helper1234);
+                        you.setText("precalculus");
+                        stuff.setText("");
                         break;
+                    case "al2":
+                        helper1234 = "What al2 course?";
+                        bot.setText(helper1234);
+                        you.setText("al2");
+                        stuff.setText("");
+                        break;
+                    case "geo":
+                        helper1234 = "What topic in geo?";
+                        bot.setText(helper1234);
+                        you.setText("geo");
+                        stuff.setText("");
+                        break;
+                    case "right triangle":
+                        helper1234 = "Please take this test to help us help you. " +
+                                "What is the sin of 30 degrees?";
+                        bot.setText(helper1234);
+                        you.setText("right triangle");
+                        stuff.setText("");
+                        break;
+                    case "0.5":
+                        bot.setText("Correct. Type \"enter\" for next question.");
+                        you.setText("0.5");
+                        stuff.setText("");
+                        break;
+                    case "enter":
+                        bot.setText("What is the arcsin of 1?");
+                        you.setText("0.5");
+                        stuff.setText("");
+                        break;
+                    case "90":
+                        bot.setText("Correct. Type \"enter1\" for next question.");
+                        you.setText("90");
+                        stuff.setText("");
+                        break;
+                    case "enter1":
+                        bot.setText("What is the tan of 60 degrees? Type \"sqrt\" for square root.");
+                        you.setText("enter1");
+                        stuff.setText("");
+                        break;
+                    case "sqrt":
+                        bot.setText("Correct. Type \"enter2\" for the next question.");
+                        you.setText("sqrt3");
+                        stuff.setText("");
+                        break;
+                    default:
+                        helper1234 = "Not understood";
+                        bot.setText(helper1234);
+                        stuff.setText("");
+                        break;
+
                 }
 
-                if (helper1234.equals("What math course?"))
-                {
-                    String helper123 = " ";
-                    switch (stuffin){
-                        case "precalculus": helper123 = "What precalculus course?";
-                            bot.setText(helper123);
-                            break;
-                        case "al2": helper123 = "What al2 course?";
-                            bot.setText(helper123);
-                            break;
-                        case "geo": helper123 = "What topic in geo?";
-                            bot.setText(helper123);
-                            break;
-                        default:helper123 = "Not understood";
-                            break;
-                    }
-                }
 
 
 
 
-
-
-
-            }
-
-
-
-            ;
-
-
-        });
-    }
+        }
+    });
 }
+}
+
+
+
+
