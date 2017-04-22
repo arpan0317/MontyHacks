3932a62cff665b943c53aa98eb499d57ced1c3c0
bho <brendon.ho01@gmail.com>
2017-04-22 17:05:44 -0400

    ayyyy

--- app/src/main/java/com/brendonho/studybuddy/Bot.java
+++ app/src/main/java/com/brendonho/studybuddy/Bot.java
@@ -32,8 +32,6 @@ public class Bot extends AppCompatActivity {
 
 
 
-
-
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,7 +47,43 @@ public class Bot extends AppCompatActivity {
                         you.setText("math");
                         stuff.setText("");
                         break;
+                    case "Math":
+                        helper1234 = "What math course?";
+                        bot.setText(helper1234);
+                        you.setText("math");
+                        stuff.setText("");
+                        break;
+                    case "math ":
+                        helper1234 = "What math course?";
+                        bot.setText(helper1234);
+                        you.setText("math");
+                        stuff.setText("");
+                        break;
+                    case "Math ":
+                        helper1234 = "What math course?";
+                        bot.setText(helper1234);
+                        you.setText("math");
+                        stuff.setText("");
+                        break;
                     case "science":
+                        helper1234 = "What science course?";
+                        bot.setText(helper1234);
+                        you.setText("science");
+                        stuff.setText("");
+                        break;
+                    case "Science":
+                        helper1234 = "What science course?";
+                        bot.setText(helper1234);
+                        you.setText("science");
+                        stuff.setText("");
+                        break;
+                    case "science ":
+                        helper1234 = "What science course?";
+                        bot.setText(helper1234);
+                        you.setText("science");
+                        stuff.setText("");
+                        break;
+                    case "Science ":
                         helper1234 = "What science course?";
                         bot.setText(helper1234);
                         you.setText("science");
@@ -61,20 +95,86 @@ public class Bot extends AppCompatActivity {
                         you.setText("english");
                         stuff.setText("");
                         break;
+                    case "English":
+                        helper1234 = "What topic in english?";
+                        bot.setText(helper1234);
+                        you.setText("english");
+                        stuff.setText("");
+                        break;
+                    case "english ":
+                        helper1234 = "What topic in english?";
+                        bot.setText(helper1234);
+                        you.setText("english");
+                        stuff.setText("");
+                        break;
+                    case "English ":
+                        helper1234 = "What topic in english?";
+                        bot.setText(helper1234);
+                        you.setText("english");
+                        stuff.setText("");
+                        break;
                     case "precalculus":
-                        helper1234 = "What precalculus course?";
+                        helper1234 = "What precalculus topic?";
+                        bot.setText(helper1234);
+                        you.setText("precalculus");
+                        stuff.setText("");
+                        break;
+                    case "Precalculus":
+                        helper1234 = "What precalculus topic?";
+                        bot.setText(helper1234);
+                        you.setText("precalculus");
+                        stuff.setText("");
+                        break;
+                    case "precalc":
+                        helper1234 = "What precalculus topic?";
+                        bot.setText(helper1234);
+                        you.setText("precalculus");
+                        stuff.setText("");
+                        break;
+                    case "Precalc":
+                        helper1234 = "What precalculus topic?";
                         bot.setText(helper1234);
                         you.setText("precalculus");
                         stuff.setText("");
                         break;
                     case "al2":
-                        helper1234 = "What al2 course?";
+                        helper1234 = "What al2 topic?";
+                        bot.setText(helper1234);
+                        you.setText("al2");
+                        stuff.setText("");
+                        break;
+                    case "algebra 2":
+                        helper1234 = "What al2 topic?";
+                        bot.setText(helper1234);
+                        you.setText("al2");
+                        stuff.setText("");
+                        break;
+                    case "Al2":
+                        helper1234 = "What al2 topic?";
+                        bot.setText(helper1234);
+                        you.setText("al2");
+                        stuff.setText("");
+                        break;
+                    case "Algebra 2":
+                        helper1234 = "What al2 topic?";
                         bot.setText(helper1234);
                         you.setText("al2");
                         stuff.setText("");
                         break;
                     case "geo":
-                        helper1234 = "What topic in geo?";
+                        helper1234 = "What topic in geometry?";
+                        bot.setText(helper1234);
+                        you.setText("geo");
+                        stuff.setText("");
+                        break;
+                    case "geometry":
+                        helper1234 = "What topic in geometry?";
+                        bot.setText(helper1234);
+                        you.setText("geo");
+                        stuff.setText("");
+                        break;
+                    case "Geometry":
+                        helper1234 = "What topic in geometry?";
                         bot.setText(helper1234);
                         you.setText("geo");
                         stuff.setText("");
@@ -84,6 +184,43 @@ public class Bot extends AppCompatActivity {
                                 "What is the sin of 30 degrees?";
                         bot.setText(helper1234);
                         you.setText("right triangle");
+                        stuff.setText("");
+                        break;
+                    case "Right Triangle":
+                        helper1234 = "Please take this test to help us help you. " +
+                                "What is the sin of 30 degrees?";
+                        bot.setText(helper1234);
+                        you.setText("right triangle");
+                        stuff.setText("");
+                        break;
+                    case ".5":
+                        bot.setText("Correct. Type \"enter\" for next question.");
+                        you.setText("0.5");
+                        stuff.setText("");
+                        break;
+                    case "1/2":
+                        bot.setText("Correct. Type \"enter\" for next question.");
+                        you.setText("0.5");
+                        stuff.setText("");
+                        break;
+                    case "0.1":
+                        bot.setText("Incorrect. The correct answer is 0.5. This is because sin is opposite over hypotenuse. On the special right triangle, the sides are 1 as the shortest side, square root 3 as the middle side, and 2 for the hypotenuse. Since 30 degrees comes in between the middle side and the hypotenuse, opposite over hypotenuse would give you 1/2 or 0.5.Type \"enter\" for next question.");
+                        you.setText("0.5");
+                        stuff.setText("");
+                        break;
+                    case "0.2":
+                        bot.setText("Incorrect. The correct answer is 0.5. This is because sin is opposite over hypotenuse. On the special right triangle, the sides are 1 as the shortest side, square root 3 as the middle side, and 2 for the hypotenuse. Since 30 degrees comes in between the middle side and the hypotenuse, opposite over hypotenuse would give you 1/2 or 0.5.Type \"enter\" for next question.");
+                        you.setText("0.5");
+                        stuff.setText("");
+                        break;
+                    case "0.4":
+                        bot.setText("Incorrect. The correct answer is 0.5. This is because sin is opposite over hypotenuse. On the special right triangle, the sides are 1 as the shortest side, square root 3 as the middle side, and 2 for the hypotenuse. Since 30 degrees comes in between the middle side and the hypotenuse, opposite over hypotenuse would give you 1/2 or 0.5.Type \"enter\" for next question.");
+                        you.setText("0.5");
+                        stuff.setText("");
+                        break;
+                    case "0.0":
+                        bot.setText("Incorrect. The correct answer is 0.5. This is because sin is opposite over hypotenuse. On the special right triangle, the sides are 1 as the shortest side, square root 3 as the middle side, and 2 for the hypotenuse. Since 30 degrees comes in between the middle side and the hypotenuse, opposite over hypotenuse would give you 1/2 or 0.5.Type \"enter\" for next question.");
+                        you.setText("0.5");
                         stuff.setText("");
                         break;
                     case "0.5":
@@ -96,18 +233,63 @@ public class Bot extends AppCompatActivity {
                         you.setText("0.5");
                         stuff.setText("");
                         break;
-                    case "90":
-                        bot.setText("Correct. Type \"enter1\" for next question.");
-                        you.setText("90");
-                        stuff.setText("");
-                        break;
                     case "enter1":
                         bot.setText("What is the tan of 60 degrees? Type \"sqrt\" for square root.");
                         you.setText("enter1");
                         stuff.setText("");
                         break;
-                    case "sqrt":
+                    case "90":
+                        bot.setText("Correct. Type \"enter1\" for the next question.");
+                        you.setText("90");
+                        stuff.setText("");
+                        break;
+                    case "80":
+                        bot.setText("Incorrect. The correct answer is 90. Arcsin is asking for what angle creates the given measurement. In this case, the given measurenment is 1. This means that it is on the y axis at a height of 1. We know that the special right triangle that contains the value of 1 is the 45 45 90 triangle. The angle that creates a 1 by using oppoosite over adjacent is 90 degrees. Type \"enter1\" for the next question.");
+                        you.setText("90");
+                        stuff.setText("");
+                        break;
+                    case "70":
+                        bot.setText("Incorrect. The correct answer is 90. Arcsin is asking for what angle creates the given measurement. In this case, the given measurenment is 1. This means that it is on the y axis at a height of 1. We know that the special right triangle that contains the value of 1 is the 45 45 90 triangle. The angle that creates a 1 by using oppoosite over adjacent is 90 degrees. Type \"enter1\" for the next question.");
+                        you.setText("90");
+                        stuff.setText("");
+                        break;
+                    case "60":
+                        bot.setText("Incorrect. The correct answer is 90. Arcsin is asking for what angle creates the given measurement. In this case, the given measurenment is 1. This means that it is on the y axis at a height of 1. We know that the special right triangle that contains the value of 1 is the 45 45 90 triangle. The angle that creates a 1 by using oppoosite over adjacent is 90 degrees. Type \"enter1\" for the next question.");
+                        you.setText("90");
+                        stuff.setText("");
+                        break;
+                    case "50":
+                        bot.setText("Incorrect. The correct answer is 90. Arcsin is asking for what angle creates the given measurement. In this case, the given measurenment is 1. This means that it is on the y axis at a height of 1. We know that the special right triangle that contains the value of 1 is the 45 45 90 triangle. The angle that creates a 1 by using oppoosite over adjacent is 90 degrees. Type \"enter1\" for the next question.");
+                        you.setText("90");
+                        stuff.setText("");
+                        break;
+                    case "40":
+                        bot.setText("Incorrect. The correct answer is 90. Arcsin is asking for what angle creates the given measurement. In this case, the given measurenment is 1. This means that it is on the y axis at a height of 1. We know that the special right triangle that contains the value of 1 is the 45 45 90 triangle. The angle that creates a 1 by using oppoosite over adjacent is 90 degrees. Type \"enter1\" for the next question.");
+                        you.setText("90");
+                        stuff.setText("");
+                        break;
+                    case "30":
+                        bot.setText("Incorrect. The correct answer is 90. Arcsin is asking for what angle creates the given measurement. In this case, the given measurenment is 1. This means that it is on the y axis at a height of 1. We know that the special right triangle that contains the value of 1 is the 45 45 90 triangle. The angle that creates a 1 by using oppoosite over adjacent is 90 degrees. Type \"enter1\" for the next question.");
+                        you.setText("90");
+                        stuff.setText("");
+                        break;
+                    case "sqrt3":
                         bot.setText("Correct. Type \"enter2\" for the next question.");
+                        you.setText("sqrt3");
+                        stuff.setText("");
+                        break;
+                    case "sqrt1":
+                        bot.setText("Incorrect. Tangent is opposite over adjacent. On the special right triangle, the sides are 1 as the shortest side, square root 3 as the middle side, and 2 for the hypotenuse. Since 60 degrees comes in between the short side and the hypotenuse, tangent over adjacent would give you square root 3 over 1.Type \"enter2\" for the next question.");
+                        you.setText("sqrt3");
+                        stuff.setText("");
+                        break;
+                    case "sqrt2":
+                        bot.setText("Incorrect. Tangent is opposite over adjacent. On the special right triangle, the sides are 1 as the shortest side, square root 3 as the middle side, and 2 for the hypotenuse. Since 60 degrees comes in between the short side and the hypotenuse, tangent over adjacent would give you square root 3 over 1.Type \"enter2\" for the next question.");
+                        you.setText("sqrt3");
+                        stuff.setText("");
+                        break;
+                    case "sqrt4":
+                        bot.setText("Incorrect. Tangent is opposite over adjacent. On the special right triangle, the sides are 1 as the shortest side, square root 3 as the middle side, and 2 for the hypotenuse. Since 60 degrees comes in between the short side and the hypotenuse, tangent over adjacent would give you square root 3 over 1.Type \"enter2\" for the next question." + );
                         you.setText("sqrt3");
                         stuff.setText("");
                         break;
