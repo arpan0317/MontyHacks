6df91feaf86e1244e4053af4334e127e5e913ed7
bho <brendon.ho01@gmail.com>
2017-04-22 11:07:49 -0400

    ayy

--- app/src/main/java/com/brendonho/studybuddy/Bot.java
+++ app/src/main/java/com/brendonho/studybuddy/Bot.java
@@ -2,6 +2,12 @@ package com.brendonho.studybuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class Bot extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class Bot extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bot);
+
+        ListView listView = (ListView) findViewById(R.id.listview);
+        final EditText stuff = (EditText) findViewById(R.id.editText);
+        Button send = (Button) findViewById(R.id.button);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stuffin = stuff.getText().toString();
+            }
+        });
+
     }
 }
